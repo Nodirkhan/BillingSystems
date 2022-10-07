5c0914724f40f4105fde0a73bd35cae399e196e9
Nodirkhan <abdumurodovnodirxon@gmail.com>
2022-10-08 00:51:53 +0500

    Add dockerfile

--- Billing.Authentication.Api/Program.cs
+++ Billing.Authentication.Api/Program.cs
@@ -20,7 +20,7 @@ namespace Billing.Authentication.Api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>();                   
                 });
     }
 }

db5612a9ed65463df49cc4856fd2f6edf199e1e7
Nodirkhan <abdumurodovnodirxon@gmail.com>
2022-10-08 01:10:44 +0500

    add tests for authentication service

---
+++ Billing.Authentication.Business.Test/UnitTest1.cs
@@ -0,0 +1,76 @@
+using Billing.Authentication.Business.Services;
+using Billing.Authentication.Domains.Entities;
+using Billing.Authentication.Domains.Models;
+using Billing.Authentication.Infrastructure.Interface;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Billing.Authentication.Business.Test
+{
+    public class AuthenticationTest
+    {
+        private Mock<IUserRepositoryAsync> _userRepository = new Mock<IUserRepositoryAsync>();
+        private UserServiceAsync _userService;
+        public AuthenticationTest()
+        {
+            _userService = new UserServiceAsync(_userRepository.Object);
+        }
+        [Fact]
+        public async Task LoginAsync_ShouldReturnIsSuccesTrue_WhenUserIsExist()
+        {
+            //Arrange
+
+            var login = new Login
+            {
+                UserName = "key",
+                Password = "key"
+            };
+            var user = new User
+            {
+                UserName = "Something",
+                FirstName = "Something",
+                LastName = "Something",
+                Id = Guid.NewGuid().ToString(),
+                Password = "Something",
+                Role = null
+
+            };
+            _userRepository.Setup(x =>
+                x.LoginAsync(It.IsAny<string>(), It.IsAny<string>()))
+                  .ReturnsAsync(user);
+
+            //Act
+
+            var response = await _userService.LoginAsync(login);
+
+            // Assert
+
+            Assert.True(response.IsSuccess);
+            Assert.NotNull(response.Result);
+        }
+
+        [Fact]
+        public async Task LoginAsyc_ShouldReturnFalse_WhenUseiDoesNotExist()
+        {
+            //Arrange
+            var login = new Login
+            {
+                UserName = "key",
+                Password = "key"
+            };
+
+            _userRepository.Setup(x => x.LoginAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(() => null);
+
+            //Act
+            var response = await _userService.LoginAsync(login);
+
+            //Assert
+            Assert.False(response.IsSuccess);
+            Assert.Null(response.Result);
+        }
+    }
+
+}
